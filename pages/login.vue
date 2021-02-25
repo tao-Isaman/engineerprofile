a352eb3bfa5a9f9cb3034fb5840aeb182ad6bdf0
isaman sangbamrung <towlovelove@gmail.com>
2021-02-26 00:32:47 +0700

    init firebase

---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="email"
+        type="email"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password"
+        :rules="passwordRules"
+        label="password"
+        type="password"
+        required
+      ></v-text-field>
+
+      <v-btn
+        :disabled="!valid"
+        color="success"
+        class="mr-4"
+        @click="validate"
+      >
+        login
+      </v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    email: '',
+    emailRules: [
+      (v) => !!v || 'Name is required',
+      (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    password: '',
+    passwordRules: [(v) => !!v || 'password is required'],
+  }),
+}
+</script>
